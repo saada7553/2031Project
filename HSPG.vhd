45e4bd34619468ba017122078c68c70ba6d82b6c
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2024-04-01 18:51:37 -0400

    vhdl for position and input-range control

--- HSPG.vhd
+++ HSPG.vhd
@@ -7,6 +7,7 @@ library lpm;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use lpm.lpm_components.all;
+use IEEE.numeric_std.all;
 
 -- single controller ====================================================================================================
 
@@ -27,15 +28,24 @@ entity HSPG_SERVO is
 end HSPG_SERVO;
 
 architecture a of HSPG_SERVO is
+	-- ticks (clock ticks) measured via 12MHz
+
+	constant ticks_absolute_min : unsigned(15 downto 0) := to_unsigned(7200, 16); -- 0.6ms
+	constant ticks_absolute_max : unsigned(15 downto 0) := to_unsigned(28800, 16); -- 2.4ms
+
+	constant ticks_min : unsigned(15 downto 0) := to_unsigned(12000, 16); -- 1ms -- TODO: perhaps make configurable
+	constant ticks_max : unsigned(15 downto 0) := to_unsigned(24000, 16); -- 2ms
+
+	constant ticks_period : unsigned(15 downto 0) := to_unsigned(240000, 16); -- 20ms
 
 	-- user inputs
-	signal position : std_logic_vector(15 downto 0);  -- POS (-100 to 100)?
-	signal min_pct  : std_logic_vector(15 downto 0);  -- MIN (percent 0-100)
-	signal max_pct  : std_logic_vector(15 downto 0);  -- MAX (percent 0-100)
-	signal speed    : std_logic_vector(15 downto 0);  -- SPD (0=SLOW, 1=MEDIUM, 2=FAST, 3=UNCAPPED)
+	signal position      : signed(15 downto 0);  -- POS (-100 to 100)?
+	signal min_position  : signed(15 downto 0);  -- MIN (percent 0-100)
+	signal max_position  : signed(15 downto 0);  -- MAX (percent 0-100)
+	signal speed         : std_logic_vector(15 downto 0);  -- SPD (0=SLOW, 1=MEDIUM, 2=FAST, 3=UNCAPPED)
 	 
 	-- internals
-    signal count    : std_logic_vector(15 downto 0);  -- internal counter
+    signal ticks    : unsigned(15 downto 0) := x"0000";  -- internal counter
 
 begin -- start impl
 
@@ -43,18 +53,18 @@ begin -- start impl
     process (RESETN, CS_POS, CS_MIN, CS_MAX, CS_SPD) begin
         if RESETN = '0' then
             position <= x"0000"; -- 0
-			min_pct <= x"0000";  -- 0%
-            max_pct <= x"0064";  -- 100%
+			min_position <= x"0000";  -- 0%
+            max_position <= x"0064";  -- 100%
             speed <= x"0000";    -- 0=SLOW
         elsif IO_WRITE = '1' then
 			if rising_edge(CS_POS) then
-				position <= IO_DATA;
+				position <= signed(IO_DATA);
 			end if;
 			if rising_edge(CS_MIN) then
-				min_pct <= IO_DATA;
+				min_position <= signed(IO_DATA);
 			end if;
 			if rising_edge(CS_MAX) then
-				max_pct <= IO_DATA;
+				max_position <= signed(IO_DATA);
 			end if;
 			if rising_edge(CS_SPD) then
 				speed <= IO_DATA;
@@ -62,24 +72,59 @@ begin -- start impl
         end if;
     end process;
 
-	-- pulse generator (every 200 clock cycles is 20ms)
+	-- pulse generator 12MHz clock -> (every 240000 ticks is 20ms)
+	-- 12000 ticks for 1ms (lower bound)
+	-- 24000 ticks for 2ms (upper bound)
     process (RESETN, CLOCK)
+		variable user_possibilities_raw : signed(15 downto 0);
+		variable user_possibilities : unsigned(15 downto 0);
+		variable user_position_raw : signed(15 downto 0); -- amount user is above min_position
+		variable user_position : unsigned(15 downto 0); -- amount user is above min_position
+		variable position_ticks_raw : unsigned(31 downto 0);
+		variable position_ticks : unsigned(15 downto 0);
     begin
         if (RESETN = '0') then
-            count <= x"0000";
+            ticks <= x"0000";
         elsif rising_edge(CLOCK) then
+			---------------------- find position_ticks
+			user_possibilities_raw := (max_position - min_position) + 1;
+			if (user_possibilities_raw < 0) or (user_possibilities_raw = 0) then
+				user_possibilities := x"0001"; -- TODO: one or zero?
+			else
+				user_possibilities := unsigned(user_possibilities_raw);
+			end if;
+			
+			user_position_raw := position - min_position;
+			if user_position_raw < 0 then
+				user_position := x"0000";
+			else
+				user_position := unsigned(user_position_raw);
+			end if;
+
+			-- TODO: configurable ticks_min/ticks_max?
+			position_ticks_raw := ((user_position * (ticks_max - ticks_min)) / (user_possibilities)) + ticks_min;
+
+			-- bound check just in case! (hopefully this isn't even possible/necessary)
+			if position_ticks_raw < ticks_absolute_min then
+				position_ticks := ticks_absolute_min;
+			elsif position_ticks_raw > ticks_absolute_max then
+				position_ticks := ticks_absolute_max;
+			else
+				position_ticks := resize(position_ticks_raw, 16);
+			end if;
+
             -- Each clock cycle, a counter is incremented.
-            count <= count + 1;
+            ticks <= ticks + 1;
 
             -- When the counter reaches the full desired period, start the period over.
-            if count = x"00C7" then  -- 20 ms has elapsed
+            if ticks = ticks_period then  -- 20 ms has elapsed
                 -- Reset the counter and set the output high.
-                count <= x"0000";
+                ticks <= x"0000";
                 PULSE <= '1';
 
             -- Within the period, when the counter reaches the "position" value, set the output low.
             -- This will make larger position values produce longer pulses.
-            elsif count = position then
+            elsif ticks = position_ticks then
                 PULSE <= '0';
             end if;
         end if;
@@ -87,7 +132,6 @@ begin -- start impl
 end a;
 
 -- multiplexor ====================================================================================================
-
 library IEEE;
 library lpm;
 
--- HSPG_test.asm
+++ HSPG_test.asm
@@ -1,9 +1,15 @@
 ; An empty ASM program ...
 
 Here:
-	IN Switches
-	OUT LEDs
-	OUT HSPG
+	; IN Switches
+	; OUT LEDs
+	; OUT HSPG
+	LOADI 0
+	OUT HSPG_SEL
+
+	LOADI 100
+	OUT HSPG_POS
+
 	JUMP Here
 	
 ; IO address constants
@@ -12,4 +18,8 @@ LEDs:      EQU 001
 Timer:     EQU 002
 Hex0:      EQU 004
 Hex1:      EQU 005
-HSPG:      EQU &H50
+HSPG_SEL:      EQU &H50
+HSPG_POS:      EQU &H51
+HSPG_MIN:      EQU &H52
+HSPG_MAX:      EQU &H53
+HSPG_SPD:      EQU &H54
