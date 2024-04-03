5de73f4bb65f3ece9d9bcb9ce11a796be996e8e4
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2024-04-03 00:55:55 -0400

    start speed impl

--- HSPG.vhd
+++ HSPG.vhd
@@ -14,9 +14,9 @@ use IEEE.numeric_std.all;
 entity HSPG_SERVO is
     port(
         CS_POS      : in  std_logic;
-        CS_MIN      : in  std_logic;
-        CS_MAX      : in  std_logic;
-        CS_SPD      : in  std_logic;
+        CS_MIN_POS  : in  std_logic;
+        CS_MAX_POS  : in  std_logic;
+        CS_ROT_TIME : in  std_logic;
 		  
         IO_WRITE    : in  std_logic;
         IO_DATA     : in  std_logic_vector(15 downto 0);
@@ -33,7 +33,7 @@ architecture a of HSPG_SERVO is
 	constant ticks_absolute_min : unsigned(15 downto 0) := to_unsigned(7200, 16); -- 0.6ms
 	constant ticks_absolute_max : unsigned(15 downto 0) := to_unsigned(28800, 16); -- 2.4ms
 
-	constant ticks_min : unsigned(15 downto 0) := to_unsigned(12000, 16); -- 1ms -- TODO: perhaps make configurable
+	constant ticks_min : unsigned(15 downto 0) := to_unsigned(12000, 16); -- 1ms -- TODO: perhaps make configurable - edit, probably don't tbh
 	constant ticks_max : unsigned(15 downto 0) := to_unsigned(24000, 16); -- 2ms
 
 	constant ticks_period : unsigned(15 downto 0) := to_unsigned(240000, 16); -- 20ms
@@ -45,12 +45,14 @@ architecture a of HSPG_SERVO is
 	signal rot_time      : unsigned(15 downto 0); -- ROT_TIME (default = 0, [ms to track from MIN to MAX])
 	 
 	-- internals
-    signal ticks    : unsigned(15 downto 0) := x"0000";  -- internal counter
+    signal ticks                     : unsigned(15 downto 0) := x"0000"; -- internal counter
+    signal current_position_ticks    : unsigned(15 downto 0) := x"0000";
+    signal spd_ticks_till_move       : unsigned(15 downto 0) := x"0000";
 
 begin -- start impl
 
 	-- set pos/min/max/spd via IO
-    process (RESETN, CS_POS, CS_MIN, CS_MAX, CS_SPD) begin
+    process (RESETN, CS_POS, CS_MIN_POS, CS_MAX_POS, CS_ROT_TIME) begin
         if RESETN = '0' then
             position <= x"0000"; -- 0
 			min_position <= x"0000";  -- 0
@@ -60,13 +62,13 @@ begin -- start impl
 			if rising_edge(CS_POS) then
 				position <= signed(IO_DATA);
 			end if;
-			if rising_edge(CS_MIN) then
+			if rising_edge(CS_MIN_POS) then
 				min_position <= signed(IO_DATA);
 			end if;
-			if rising_edge(CS_MAX) then
+			if rising_edge(CS_MAX_POS) then
 				max_position <= signed(IO_DATA);
 			end if;
-			if rising_edge(CS_SPD) then
+			if rising_edge(CS_ROT_TIME) then
 				rot_time <= unsigned(IO_DATA);
 			end if;
         end if;
@@ -81,12 +83,13 @@ begin -- start impl
 		variable user_position_raw : signed(15 downto 0); -- amount user is above min_position
 		variable user_position : unsigned(15 downto 0); -- amount user is above min_position
 		variable position_ticks_raw : unsigned(31 downto 0);
-		variable position_ticks : unsigned(15 downto 0);
+		variable target_position_ticks : unsigned(15 downto 0);
+		variable needed_spd_ticks : unsigned(15 downto 0);
     begin
         if (RESETN = '0') then
             ticks <= x"0000";
         elsif rising_edge(CLOCK) then
-			---------------------- find position_ticks
+			---------------------- find target_position_ticks
 			user_possibilities_raw := (max_position - min_position) + 1;
 			if (user_possibilities_raw < 0) or (user_possibilities_raw = 0) then
 				user_possibilities := x"0001"; -- TODO: one or zero?
@@ -106,12 +109,40 @@ begin -- start impl
 
 			-- bound check just in case! (hopefully this isn't even possible/necessary)
 			if position_ticks_raw < ticks_absolute_min then
-				position_ticks := ticks_absolute_min;
+				target_position_ticks := ticks_absolute_min;
 			elsif position_ticks_raw > ticks_absolute_max then
-				position_ticks := ticks_absolute_max;
+				target_position_ticks := ticks_absolute_max;
 			else
-				position_ticks := resize(position_ticks_raw, 16);
+				target_position_ticks := resize(position_ticks_raw, 16);
 			end if;
+			------------------------------------------
+
+			------------------------------ track curr to target via speed (rot_time)
+			if rot_time = 0 or current_position_ticks = target_position_ticks then
+				current_position_ticks <= target_position_ticks;
+				spd_ticks_till_move <= x"0000";
+			else
+				-- NOTE: this code assumes we only use functional range 1-2ms!! - do not change this thusforth! (1ms matches ms units -> simple impl)
+				--    math gives that we move 1/rot_time ticks each tick
+				--    we mock this by instead moving on tick every rot_time ticks
+				needed_spd_ticks := rot_time - 1;
+
+				if spd_ticks_till_move = needed_spd_ticks or spd_ticks_till_move > needed_spd_ticks then
+					if target_position_ticks < current_position_ticks then
+						current_position_ticks <= current_position_ticks - 1;
+					else
+						current_position_ticks <= current_position_ticks + 1;
+					end if;
+
+					-- move towards target!
+					spd_ticks_till_move <= x"0000";
+				else
+					spd_ticks_till_move <= spd_ticks_till_move + 1;
+				end if;
+
+			end if;
+
+			------------------------------------------------------------------------ 
 
             -- Each clock cycle, a counter is incremented.
             ticks <= ticks + 1;
@@ -124,7 +155,7 @@ begin -- start impl
 
             -- Within the period, when the counter reaches the "position" value, set the output low.
             -- This will make larger position values produce longer pulses.
-            elsif ticks = position_ticks then
+            elsif ticks = current_position_ticks then
                 PULSE <= '0';
             end if;
         end if;
@@ -143,9 +174,9 @@ entity HSPG is
     port(
         CS_SEL      : in  std_logic;
         CS_POS      : in  std_logic;
-        CS_MIN      : in  std_logic;
-        CS_MAX      : in  std_logic;
-        CS_SPD      : in  std_logic;
+        CS_MIN_POS  : in  std_logic;
+        CS_MAX_POS  : in  std_logic;
+        CS_ROT_TIME : in  std_logic;
 		  
         IO_WRITE    : in  std_logic;
         IO_DATA     : in  std_logic_vector(15 downto 0);
@@ -164,9 +195,9 @@ architecture a of HSPG is
 	component HSPG_SERVO is
 		port(
 			CS_POS      : in  std_logic;
-			CS_MIN      : in  std_logic;
-			CS_MAX      : in  std_logic;
-			CS_SPD      : in  std_logic;
+			CS_MIN_POS  : in  std_logic;
+			CS_MAX_POS  : in  std_logic;
+			CS_ROT_TIME : in  std_logic;
 			  
 			IO_WRITE    : in  std_logic;
 			IO_DATA     : in  std_logic_vector(15 downto 0);
@@ -192,36 +223,36 @@ begin -- start impl
 
 	SERVO_0 : HSPG_SERVO port map(
 		CS_POS and en_0,
-		CS_MIN and en_0,
-		CS_MAX and en_0,
-		CS_SPD and en_0,
+		CS_MIN_POS and en_0,
+		CS_MAX_POS and en_0,
+		CS_ROT_TIME and en_0,
 		IO_WRITE, IO_DATA, CLOCK, RESETN,
 		PULSE_0
 	);
 
 	SERVO_1 : HSPG_SERVO port map(
 		CS_POS and en_1,
-		CS_MIN and en_1,
-		CS_MAX and en_1,
-		CS_SPD and en_1,
+		CS_MIN_POS and en_1,
+		CS_MAX_POS and en_1,
+		CS_ROT_TIME and en_1,
 		IO_WRITE, IO_DATA, CLOCK, RESETN,
 		PULSE_1
 	);
 
 	SERVO_2 : HSPG_SERVO port map(
 		CS_POS and en_2,
-		CS_MIN and en_2,
-		CS_MAX and en_2,
-		CS_SPD and en_2,
+		CS_MIN_POS and en_2,
+		CS_MAX_POS and en_2,
+		CS_ROT_TIME and en_2,
 		IO_WRITE, IO_DATA, CLOCK, RESETN,
 		PULSE_2
 	);
 
 	SERVO_3 : HSPG_SERVO port map(
 		CS_POS and en_3,
-		CS_MIN and en_3,
-		CS_MAX and en_3,
-		CS_SPD and en_3,
+		CS_MIN_POS and en_3,
+		CS_MAX_POS and en_3,
+		CS_ROT_TIME and en_3,
 		IO_WRITE, IO_DATA, CLOCK, RESETN,
 		PULSE_3
 	);
