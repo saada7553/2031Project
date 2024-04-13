252cecd7960f99093ce5f71fa354a31fffa1a062
Saad Ata <s.ata@fetchrewards.com>
2024-04-13 14:21:58 -0400

    New Test asm

--- HSPG_test.asm
+++ HSPG_test.asm
@@ -14,135 +14,22 @@ Init:
 	LOADI 0
 	OUT HSPG_SEL
 
-	JUMP UpdateLoop:
-
-NumServos: DW 4
-FanIndexArrPtr: DW 0
-
-FanIndexArr: // []
-	DW 500
-
-FanPosCount: DW 2
-FanPosArr: // [] -- TODO (use later)
-	DW 600
-
-; input = servo-index
-ServoIndex: DW 0
-MemTemp1: DW 0
-
-GetFanIndex: ; ServoIndex = servo-i: return --> FanIndexArr[servo-i]
-	LOAD ServoIndex
-	ADD FanIndexArr
-	STORE MemTemp1
-	LOADI MemTemp1
-	
-	RETURN
-SetFanIndex: ; input = new-fan-i, ServoIndex = servo-index
-	
-	RETURN
-UpdateFan:
-	STORE ServoIndex
+Loop: 
+	IN Switches
+	AND SelMask
 	OUT HSPG_SEL
 
-	CALL GetFanIndex
+	IN Switches
+	AND PosMask
 	OUT HSPG_POS
 
-	IN HSPG_MODE
-	JZERO NoIncFan
-		CALL GetFanIndex
-		ADDI 1 ; fan_idx++
-		SUB FanPosCount
-
-		JZERO WrapFan
-		JUMP SkipWrapFan
-		WrapFan:
-			LOADI 0
-			JUMP FanIndexDone
-		SkipWrapFan:
-			ADD FanPosCount
-		FanIndexDone:
-	NoIncFan:
-
-	CALL SetFanIndex
-	
-	RETURN
-
-; SetModeArr:
-; 	; LOAD ModeArrIndex
-; 	; SUB
-; 	RETURN
-;
-; GetModeArr:
-; 	RETURN
-
-ModeArrValue: DW &H00
-ModeArrIndex: DW &H00
-
-ModeArr0: DW &H00
-ModeArr1: DW &H00
-ModeArr2: DW &H00
-ModeArr3: DW &H00
-
-UpdateLoop:
-	LOADI 0
-	CALL UpdateFan
-
-	LOADI 1
-	CALL UpdateFan
-
-	LOADI 2
-	CALL UpdateFan
-
-	LOADI 3
-	CALL UpdateFan
-	; IN Switches
-	; AND SelMask
-	;
-	; JNEG Sel_1
-	; JPOS Sel_1
-	; Sel_0:
-	; 	LOADI 0
-	; 	OUT HSPG_MIN_POS
-	;
-	; 	LOADI 100
-	; 	OUT HSPG_MAX_POS
-	;
-	; 	LOADI 0
-	; 	OUT HSPG_ROT_TIME
-	; JUMP After_Sel
-	; Sel_1:
-	; 	LOADI 0
-	; 	OUT HSPG_MIN_POS
-	;
-	; 	LOADI 15
-	; 	OUT HSPG_MAX_POS
-	;
-	; 	LOADI 1000
-	; 	OUT HSPG_ROT_TIME
-	; After_Sel:
-	; ; ----
-	;
-	; IN Switches
-	; AND PosMask
-	;
-	; OUT HEX0 ; ?
-	; OUT HEX1 ; ?
-	;
-	; OUT HSPG_POS
-	;
-	; ; show if servo is done
-	; IN HSPG_DONE
-	; OUT LEDs
-
-	JUMP UpdateLoop
-; =========================
-SelMask:      DW &B1000000000
-PosMask:      DW &B0111111111
+SelMask:      DW &B1100000000
+PosMask:      DW &B0011111111
 	
 ; IO address constants
 Switches:  EQU 000
 LEDs:      EQU 001
-; Timer:     EQU 002
+Timer:     EQU 002
 Hex0:      EQU 004
 Hex1:      EQU 005
 
@@ -152,13 +39,3 @@ HSPG_MIN_POS:       EQU &H52
 HSPG_MAX_POS:       EQU &H53
 HSPG_ROT_TIME:      EQU &H54
 HSPG_DONE:          EQU &H55
-
-ORG 500 // FanIndexArr
-DW 0
-DW 0
-DW 0
-DW 0
-
-ORG 600 // FanPosArr
-DW 0
-DW 1
